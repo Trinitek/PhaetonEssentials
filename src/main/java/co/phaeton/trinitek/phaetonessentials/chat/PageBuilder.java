2693296e854b5ff9f6186c19521b7f133a371696
Trinitek <trinitek@live.com>
2014-07-13 15:12:49 -0400

    PageBuilder uses arrays instead of ArrayLists
    Also removed the type mismatch suppression from the last commit

--- src/main/java/co/phaeton/trinitek/phaetonessentials/chat/PageBuilder.java
+++ src/main/java/co/phaeton/trinitek/phaetonessentials/chat/PageBuilder.java
@@ -2,13 +2,11 @@ package co.phaeton.trinitek.phaetonessentials.chat;
 
 import org.bukkit.command.CommandSender;
 
-import java.util.ArrayList;
-
 public class PageBuilder {
-
-    private ArrayList<String> header;
-    private ArrayList<String> body;
-    private ArrayList<String> footer;
+    
+    private String[] header;
+    private String[] body;
+    private String[] footer;
     private int sizeOfPage;
     private int numberOfPages;
     private int numberOfBodyLinesPerPage;
@@ -27,30 +25,27 @@ public class PageBuilder {
 
     /**
      * Set the header to the given ArrayList of Strings
-     * @param stringList - ArrayList of Strings that constitute the header
+     * @param stringList - array of Strings that constitute the header
      */
-    public void setHeader(ArrayList<String> stringList) {
-        if (stringList == null) stringList = new ArrayList<>();
+    public void setHeader(String[] stringList) {
         this.header = stringList;
         adjustPageSize();
     }
 
     /**
      * Set the body to the given ArrayList of Strings
-     * @param stringList - ArrayList of Strings that constitute the body
+     * @param stringList - array of Strings that constitute the header
      */
-    public void setBody(ArrayList<String> stringList) {
-        if (stringList == null) stringList = new ArrayList<>();
+    public void setBody(String[] stringList) {
         this.body = stringList;
         adjustPageSize();
     }
 
     /**
      * Set the footer to the given ArrayList of Strings
-     * @param stringList - ArrayList of Strings that constitute the footer
+     * @param stringList - array of Strings that constitute the footer
      */
-    public void setFooter(ArrayList<String> stringList) {
-        if (stringList == null) stringList = new ArrayList<>();
+    public void setFooter(String[] stringList) {
         this.footer = stringList;
         adjustPageSize();
     }
@@ -59,9 +54,9 @@ public class PageBuilder {
      * Adjust all the object's variables to account for changes in the size of the message's string arrays
      */
     private void adjustPageSize() {
-        int headerLength = this.header.size();
-        int bodyLength = this.body.size();
-        int footerLength = this.body.size();
+        int headerLength = this.header.length;
+        int bodyLength = this.body.length;
+        int footerLength = this.footer.length;
 
         // If the number of header and footer strings alone hits the page size limit, size accordingly
         if (headerLength + footerLength >= DEFAULT_PAGE_SIZE) {
@@ -88,7 +83,6 @@ public class PageBuilder {
      * @param pageNumber - page number to generate
      * @return String array of the page's contents
      */
-    @SuppressWarnings("SuspiciousSystemArraycopy")
     private String[] buildPage(int pageNumber) {
         String[] returnString = new String[this.sizeOfPage];
         int currentIndex = 0;
@@ -98,22 +92,22 @@ public class PageBuilder {
         else if (pageNumber > this.numberOfPages) pageNumber = this.numberOfPages;
 
         // Append the header
-        System.arraycopy(this.header.toArray(), 0, returnString, currentIndex, this.header.size());
-        currentIndex += this.header.size();
+        System.arraycopy(this.header, 0, returnString, currentIndex, this.header.length);
+        currentIndex += this.header.length;
 
         // Append the body
         int bodyIndex = pageNumber * this.numberOfBodyLinesPerPage - this.numberOfBodyLinesPerPage;
         int bodyLinesToShow;
 
         // If the body array has less than numberOfBodyLinesPerPage left, then only append the remaining strings
-        if ((this.body.size() - bodyIndex + 1 < this.numberOfBodyLinesPerPage))
+        if ((this.body.length - bodyIndex + 1 < this.numberOfBodyLinesPerPage))
             bodyLinesToShow = this.body.size() - bodyIndex + 1;
         else
             bodyLinesToShow = this.numberOfBodyLinesPerPage;
-        System.arraycopy(this.body.toArray(), bodyIndex, returnString, currentIndex, bodyLinesToShow);
+        System.arraycopy(this.body, bodyIndex, returnString, currentIndex, bodyLinesToShow);
 
         // Append the footer
-        System.arraycopy(this.footer.toArray(), 0, returnString, currentIndex, this.footer.size());
+        System.arraycopy(this.footer, 0, returnString, currentIndex, this.footer.length);
 
         return returnString;
     }
